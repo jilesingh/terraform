6c3000fe906bc9d11c7ad40cce9dae43f16ced8b
jilesingh <itsjilesingh@gmail.com>
2024-03-19 16:57:25 +0100

    tfvars file and variable file added

---
+++ deploying_gcp_resources/variables.tf
@@ -0,0 +1,29 @@
+### VARIABLES
+variable "project-id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+  default = "europe-west3"
+}
+
+variable "zone" {
+  type = string
+  default = "europe-west3-a"
+}
+
+variable "subnet-name" {
+  type = string
+  default = "subnet1"
+}
+
+variable "subnet-cidr" {
+  type = string
+  default = "10.127.0.0/20"
+}
+
+variable "private_google_access" {
+  type = bool
+  default = true
+}
