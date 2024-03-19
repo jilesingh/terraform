638ef0af5456c2d706a84a9e5962d532e0cfa5d2
jilesingh <itsjilesingh@gmail.com>
2024-03-19 18:20:28 +0100

    variables added to 02_01

--- deploying_gcp_resources/main.tf
+++ deploying_gcp_resources/main.tf
@@ -12,11 +12,11 @@ data "google_compute_network" "default" {
 
 ## SUBNET
 resource "google_compute_subnetwork" "subnet-1" {
-  name                            = "subnet1"
-  ip_cidr_range                   = "10.127.0.0/20"
-  network                         = data.google_compute_network.default.self_link
-  region                          = "europe-west3"
-  private_ip_google_access        = true
+  name                     = var.subnet-name
+  ip_cidr_range            = var.subnet-cidr
+  network                  = data.google_compute_network.default.self_link
+  region                   = var.region
+  private_ip_google_access = var.private_google_access
 }
 
 resource "google_compute_firewall" "default" {
