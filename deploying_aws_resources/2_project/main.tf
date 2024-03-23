d1bd3ea35136b1e52828bc0e57698bf74adb2e3b
jilesingh <itsjilesingh@gmail.com>
2024-03-23 10:24:30 +0100

    application deployed

--- deploying_aws_resources/2_project/main.tf
+++ deploying_aws_resources/2_project/main.tf
@@ -10,5 +10,40 @@ terraform {
 }
 
 provider "aws" {
-  region  = "eu-central-1"
+  region = "eu-central-1"
+}
+
+resource "aws_security_group" "app_sg" {
+  name        = "app_sg"
+  description = "allow on port 8080"
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.app_sg.id
+  from_port         = 8080
+  ip_protocol       = "tcp"
+  to_port           = 8080
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.app_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+  name_regex  = "cocktails-app-*"
+  owners      = ["self"]
+
+}
+resource "aws_instance" "web_app" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.app_ami.id
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+
 }
